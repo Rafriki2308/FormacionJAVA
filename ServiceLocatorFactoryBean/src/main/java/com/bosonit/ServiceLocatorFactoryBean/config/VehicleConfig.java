736140dcebc745cb31ab7de583edb095f7a4de19
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-14 11:29:31 +0200

    Added VehicleConfig class

---
+++ ServiceLocatorFactoryBean/src/main/java/com/bosonit/ServiceLocatorFactoryBean/config/VehicleConfig.java
@@ -0,0 +1,19 @@
+package com.bosonit.ServiceLocatorFactoryBean.config;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.imageio.spi.ServiceRegistry;
+
+@Configuration
+public class VehicleConfig {
+
+    @Bean
+    public FactoryBean<?> factoryBean(){
+        final ServiceLocatorFactoryBean bean = new ServiceLocatorFactoryBean();
+        bean.setServiceLocatorInterface(ServiceRegistry.class);
+        return bean;
+    }
+}
