7902ed0031f0fcbdf63c609710f78999f20e9045
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-14 11:27:10 +0200

    Added controller class

---
+++ ServiceLocatorFactoryBean/src/main/java/com/bosonit/ServiceLocatorFactoryBean/controller/VehicleController.java
@@ -0,0 +1,22 @@
+package com.bosonit.ServiceLocatorFactoryBean.controller;
+
+import com.bosonit.ServiceLocatorFactoryBean.model.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.imageio.spi.ServiceRegistry;
+
+@RestController
+@RequestMapping("/vehicle")
+public class VehicleController {
+
+    @Autowired
+    private ServiceRegistry serviceRegistry;
+
+    @PostMapping
+    public void processByTypeOfVehicle(@RequestBody Vehicle vehicle){
+        serviceRegistry.getClass(vehicle.getVehicleType()).process(vehicle);
+    }
+}
