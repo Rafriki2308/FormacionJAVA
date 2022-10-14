97a6bc80a8358bde9952b854df1e6dcb18999b46
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-14 11:13:30 +0200

    Added Model and services class

---
+++ ServiceLocatorFactoryBean/src/main/java/com/bosonit/ServiceLocatorFactoryBean/ServiceLocatorFactoryBeanApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.ServiceLocatorFactoryBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLocatorFactoryBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLocatorFactoryBeanApplication.class, args);
+	}
+
+}
