4f3d600668d8d08c4c68486ee250f90ec8ec06f4
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-14 12:14:21 +0200

    Added VehicleController class

--- ServiceLocatorFactoryBean/src/main/java/com/bosonit/ServiceLocatorFactoryBean/controller/VehicleController.java
+++ ServiceLocatorFactoryBean/src/main/java/com/bosonit/ServiceLocatorFactoryBean/controller/VehicleController.java
@@ -1,12 +1,13 @@
 package com.bosonit.ServiceLocatorFactoryBean.controller;
 
 import com.bosonit.ServiceLocatorFactoryBean.model.Vehicle;
+import com.bosonit.ServiceLocatorFactoryBean.registry.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import javax.imageio.spi.ServiceRegistry;
+
 
 @RestController
 @RequestMapping("/vehicle")
@@ -17,6 +18,6 @@ public class VehicleController {
 
     @PostMapping
     public void processByTypeOfVehicle(@RequestBody Vehicle vehicle){
-        serviceRegistry.getClass(vehicle.getVehicleType()).process(vehicle);
+        serviceRegistry.getService(vehicle.getVehicleType()).process(vehicle);
     }
 }
