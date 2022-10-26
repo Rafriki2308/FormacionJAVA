ea9bc7fe37f8531c544e557c33c918100c3c30e3
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-26 17:40:21 +0200

    Added FileDBServiceImpl class

---
+++ ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/file/application/FileDBServiceImpl.java
@@ -0,0 +1,36 @@
+package com.bosonit.ej13.uploaddownloadfile.file.application;
+
+import com.bosonit.ej13.uploaddownloadfile.file.domain.FileDB;
+import com.bosonit.ej13.uploaddownloadfile.file.infraestructure.repository.FileDBRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+@Service
+public class FileDBServiceImpl implements FileDBService{
+
+    @Autowired
+    private FileDBRepository fR;
+
+    public FileDB store(MultipartFile file) throws IOException{
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        FileDB fileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+
+        return fR.save(fileDB);
+    }
+
+    public FileDB getFile(String id){
+        return fR.getReferenceById(id);
+    }
+
+    public Stream<FileDB> getAllFiles(){
+        return fR.findAll().stream();
+    }
+
+
+
+}
