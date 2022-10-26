00a4d8cb51184f024d7624af9ae236ca1760bc80
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-26 17:39:53 +0200

    Updated FileDB class

--- ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/file/domain/FileDB.java
+++ ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/file/domain/FileDB.java
@@ -1,13 +1,14 @@
 package com.bosonit.ej13.uploaddownloadfile.file.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 @Table(name = "files")
@@ -22,8 +23,13 @@ public class FileDB {
 
     private String category;
 
-    private Date uploadDate;
-
     @Lob
     private byte[] data;
+
+    public FileDB(String name, String category, byte[] data) {
+        this.name = name;
+        this.category = category;
+        this.data = data;
+    }
+
 }
