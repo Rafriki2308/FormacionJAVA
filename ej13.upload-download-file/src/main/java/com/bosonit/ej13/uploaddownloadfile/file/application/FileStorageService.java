69d9fad2f3c045902da6fdc01e73cce94156c4d2
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-31 10:34:59 +0100

    Updated FileStorageService interface and FileStorageServiceImp class in order to add delete all and path attribute from application.properties

--- ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/file/application/FileStorageService.java
+++ ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/file/application/FileStorageService.java
@@ -1,17 +1,18 @@
 package com.bosonit.ej13.uploaddownloadfile.file.application;
 
+import com.bosonit.ej13.uploaddownloadfile.file.domain.FileDB;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.nio.file.Path;
-import java.util.stream.Stream;
 
 public interface FileStorageService {
 
     public void init();
 
-    public void save(MultipartFile file);
+    public FileDB save(MultipartFile file, String type) throws Exception;
 
     public Resource loadByName(String filename);
 
+    public Resource loadById(String id);
+
+    public void deleteAll();
 }
--- ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/file/application/FileStorageServiceImp.java
+++ ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/file/application/FileStorageServiceImp.java
@@ -1,8 +1,14 @@
 package com.bosonit.ej13.uploaddownloadfile.file.application;
 
+import com.bosonit.ej13.uploaddownloadfile.exception.FileUploadExceptionAdvice;
+import com.bosonit.ej13.uploaddownloadfile.file.domain.FileDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -12,32 +18,47 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Service
-public class FileStorageServiceImp implements FileStorageService{
+public class FileStorageServiceImp implements FileStorageService {
 
-    private final Path root = Paths.get("uploads");
+    @Value("${file.path}")
+    private Path root;
+    @Autowired
+    private FileDBServiceImpl fileDBService;
 
     @Override
     public void init() {
         try {
             Files.createDirectory(root);
+
         } catch (IOException e) {
             throw new RuntimeException("Could not initialize folder for upload!");
         }
     }
 
     @Override
-    public void save(MultipartFile file) {
-        try {
-            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
-        } catch (Exception e) {
-            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+    public FileDB save(MultipartFile file, String type) {
+        String name = StringUtils.cleanPath(file.getContentType());
+        String[] strings = name.split("/");
+        String extension = strings[1];
+        if (extension.equals(type)) {
+            try {
+                Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+                return fileDBService.store(file, root);
+
+            } catch (Exception e) {
+                throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+            }
+
         }
+        throw new RuntimeException("Category file not matches");
     }
+
 
     @Override
     public Resource loadByName(String filename) {
         try {
             Path file = root.resolve(filename);
+
             Resource resource = new UrlResource(file.toUri());
 
             if (resource.exists() || resource.isReadable()) {
@@ -50,4 +71,28 @@ public class FileStorageServiceImp implements FileStorageService{
         }
     }
 
+    @Override
+    public Resource loadById(String id) {
+        try {
+            String filename = fileDBService.
+                    getFileById(id).
+                    getName();
+            Path file = root.resolve(filename);
+
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
 }
