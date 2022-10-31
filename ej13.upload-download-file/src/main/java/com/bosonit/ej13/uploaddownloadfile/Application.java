829c2de9ce4ca3d22ade3d303713fab4e64c1d35
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-10-31 10:37:42 +0100

    Updated run method to deploy init and deleteAll method

--- ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/Application.java
+++ ej13.upload-download-file/src/main/java/com/bosonit/ej13/uploaddownloadfile/Application.java
@@ -1,13 +1,24 @@
 package com.bosonit.ej13.uploaddownloadfile;
 
+import com.bosonit.ej13.uploaddownloadfile.file.application.FileStorageServiceImp;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    @Resource
+    FileStorageServiceImp storageService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        storageService.deleteAll();
+        storageService.init();
+    }
 }
