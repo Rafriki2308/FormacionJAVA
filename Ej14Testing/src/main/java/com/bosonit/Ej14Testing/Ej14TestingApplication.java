f59342e5fa4ab33fef483ceff795b3f2888db0b6
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-02 12:39:06 +0100

    Added Ej14Testing project

---
+++ Ej14Testing/src/main/java/com/bosonit/Ej14Testing/Ej14TestingApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.Ej14Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej14TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej14TestingApplication.class, args);
+	}
+
+}
