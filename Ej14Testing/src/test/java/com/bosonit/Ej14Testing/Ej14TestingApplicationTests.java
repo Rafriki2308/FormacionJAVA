fbcc5dd5569b5d56d321bdbea75efdd3ccec9f6d
rafael.martinez <rafael.martinez.8378@hotmail.es>
2022-11-04 10:22:09 +0100

    Added Ej14TestingApplicationTest class

---
+++ Ej14Testing/src/test/java/com/bosonit/Ej14Testing/Ej14TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.Ej14Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej14TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
